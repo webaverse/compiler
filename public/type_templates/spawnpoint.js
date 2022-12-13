085fab5fc971b28881ee701b0a98e44e02e5aba2
arya <64514807+aryasalehi@users.noreply.github.com>
2022-12-13 14:30:57 +0330

    Revert "spawnpoint wait for default player invitation event. fixes app issue 369"

--- public/type_templates/spawnpoint.js
+++ public/type_templates/spawnpoint.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp, useLocalPlayer, usePartyManager, usePlayersManager} = metaversefile;
+const {useApp, useLocalPlayer, usePartyManager} = metaversefile;
 
 const localEuler = new THREE.Euler(0, 0, 0, 'YXZ');
 
@@ -52,15 +52,13 @@ export default e => {
         const diff = new THREE.Vector3();
         const playerPosition = new THREE.Vector3();
         const partyManager = usePartyManager();
-        partyManager.addEventListener("defaultplayerinvited", ()=>{
-          const partyMembers = partyManager.getPartyPlayers();
+        const partyMembers = partyManager.getPartyPlayers();
 
-          diff.subVectors(position, localPlayer.position);
-          for (const player of partyMembers) {
-            playerPosition.addVectors(diff, player.position);
-            player.setSpawnPoint(playerPosition, quaternion);
-          }
-        });
+        diff.subVectors(position, localPlayer.position);
+        for (const player of partyMembers) {
+          playerPosition.addVectors(diff, player.position);
+          player.setSpawnPoint(playerPosition, quaternion);
+        }
       }
     })();
   }
